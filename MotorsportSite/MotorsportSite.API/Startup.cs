aa217aa721d2ffb5490c97de7fd34cda1bbe0b77
Andrew Rust <rusty88a@me.com>
2020-04-09 12:42:09 +0100

    API setup
    Adding in swagger and changes to the startup to get it to run

--- MotorsportSite/MotorsportSite.API/Startup.cs
+++ MotorsportSite/MotorsportSite.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace MotorsportSite.API
 {
@@ -26,6 +27,11 @@ namespace MotorsportSite.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MotorsportSite Api", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,8 +42,18 @@ namespace MotorsportSite.API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
 
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MotorsportSite Api");
+            });
+
+            app.UseHttpsRedirection();
+                     
             app.UseRouting();
 
             app.UseAuthorization();
