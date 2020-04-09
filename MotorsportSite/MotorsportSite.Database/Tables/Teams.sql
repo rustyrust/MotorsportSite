09006ddb1ab630f9ded4ce484cdc374260f93290
Andrew Rust <rusty88a@me.com>
2020-04-09 13:03:10 +0100

    Database Setup
    Create the first table and make a publish script

---
+++ MotorsportSite/MotorsportSite.Database/Tables/Teams.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Teams]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TeamName] NVARCHAR(100) NOT NULL, 
+    [EntryDate] DATETIME2 NOT NULL, 
+    [LeaveDate] DATETIME2 NULL, 
+    [TeamColours] NVARCHAR(255) NOT NULL
+)
