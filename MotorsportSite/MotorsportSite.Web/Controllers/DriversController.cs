1c0dbc709a37aefafbb70c43518543c26e992af2
Andrew Rust <rusty88a@me.com>
2020-04-14 20:58:01 +0100

    Web Setup for a Drivers Page
    setup the folders and files needed to be able to make
    a Drivers Page

---
+++ MotorsportSite/MotorsportSite.Web/Controllers/DriversController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MotorsportSite.Web.Controllers
+{
+    public class DriversController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
