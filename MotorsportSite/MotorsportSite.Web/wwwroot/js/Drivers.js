06335d79aa076cd72683d66253feb270c097eab2
Andrew Rust <rusty88a@me.com>
2020-04-15 16:49:13 +0100

    Adding some JavaScript
    my first attempt at making a Fetch promise to call my
    GET all drivers from my API.
    Also using truthy to chage a h1 using the DOM to a new
    colour
    Plus alter the header in layout to have the paths I want for my
    site.

--- MotorsportSite/MotorsportSite.Web/wwwroot/js/Drivers.js
+++ MotorsportSite/MotorsportSite.Web/wwwroot/js/Drivers.js
@@ -1,1 +1,24 @@
-﻿
+﻿console.log("Hello Drivers");
+
+let drivers;
+
+fetch("https://randomuser.me/api/?results=10")
+    .then((response) => response.json())
+    .then(function (data) {
+        drivers = data.results;
+        console.log(drivers);
+        changeColour()
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+function changeColour() {
+    if (drivers) {
+        document.querySelector("h1").style.color = "red";
+        console.log("im in the if");
+    }
+};
+
+
+
+
