cc1923c73100e180ab489ecb435a3612e6ea8323
Andrew Rust <rusty88a@me.com>
2020-05-04 20:11:26 +0100

    update the Drivers page
    change the vue to pull in the data from the API call
    and changes to the CSS

--- MotorsportSite/MotorsportSite.Web/wwwroot/js/Drivers.js
+++ MotorsportSite/MotorsportSite.Web/wwwroot/js/Drivers.js
@@ -1,24 +1,23 @@
 ﻿console.log('Hello Drivers');
 
 
-let driverData;
-
-fetch('https://LocalHost:44374/api/Drivers')
-    .then((response) => response.json())
-    .then(function (data) {
-        driverData = data;
-        console.log(driverData);
-    }).catch(function (error) {
-        console.log(error);
-    });
-
-
-
-let drivers = new Vue({
-    el: '#drivers',
+new Vue({
+    el: '#drivers-vueapp',
     data: {
-        driverNames: ['Hamilton', 'Vettel', 'Norris', 'Ricardo']
+        drivers: null
+    },
+    mounted: function () {
+        let self = this;
+        fetch('https://LocalHost:44374/api/Drivers')
+            .then((response) => response.json())
+            .then(function (data) {
+                self.drivers = data;
+            }).catch(function (error) {
+                console.log(error);
+            });
     }
+
+
 })
 
 
